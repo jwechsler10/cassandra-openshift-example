e8bc66eecea31e28d31544cabf50bb4f848f77f6
jwechsler10 <steamacct2@gmail.com>
2018-02-12 10:40:42 -0500

    Create webservice.js

---
+++ webservice.js
@@ -0,0 +1,13 @@
+const getAllUsers = (req, res) => {
+
+        client.execute("SELECT * FROM users, (err, result) => {
+        if (err) 
+          return res.json(err);
+              
+        return res.json(result.rows);
+         }); 
+}
+
+module.exports = {
+  getAllUsers
+};

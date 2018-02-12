a18025ee182d95e379f8a04e6613d8579ebaba90
jwechsler10 <steamacct2@gmail.com>
2018-02-12 10:40:34 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const db = require('./index.js');
+const api = require('./webservice.js);
+
+const app = express();
+const conn = db.client;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', getAllEntries);
+
+app.listen(8080, '0.0.0.0');
+console.log("Server running on port 8080");

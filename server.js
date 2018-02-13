0c4818c1a4c72f4c74abd94f6ef9804d479f979d
Jared <jwechsler10@gmail.com>
2018-02-13 14:13:55 -0500

    added get by lastname and creating user

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const api = require('./webservice.js');
 const app = express();
 const conn = db.client;
 const getAllEntries = api.getAllUsers;
+const getUserByLastName = api.getUserByLastName;
+const createUser = api.createUser;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', getAllEntries);
+app.get('/user/:lastname?', getUserByLastName);
+app.post('/user', createUser);
 
 app.listen(8080, '0.0.0.0');
 console.log("Server running on port 8080");
--- webservice.js
+++ webservice.js
@@ -2,15 +2,44 @@ const db = require('./index.js');
 const client = db.client;
 
 const getAllUsers = (req, res) => {
-
-        client.execute("SELECT * FROM users.users", (err, result) => {
-        if (err) 
-          return res.json(err);
+  client.execute("SELECT * FROM users.users", (err, result) => {
+    if (err) 
+      return res.json(err);
               
-        return res.json(result.rows);
+    return res.json(result.rows);
          }); 
 }
 
+const getUserByLastName = (req, res) => {
+  const lastName = req.query.lastname;
+  client.execute("SELECT * FROM users.users WHERE lastname = " + lastname, (err, result) => {
+
+  if (err)
+    return res.json(err);
+  
+  return res.json(result.rows);
+});
+}
+
+const createUser = (req, res) => {
+  const lastname = req.body.lastname;
+  const age = req.body.age;
+  const city = req.body.city;
+  const email = req.body.email;
+  const firstname = req.body.firstname;
+
+  client.execute("INSERT INTO users.users (id, lastname, age, city, email, firstname) VALUES " + " ( " +
+    "uuid(), " + lastname ", " + age + ", " + city + ", " + email + ", " + firstname + ")", (err, result) => {
+    if(err)
+      return res.json(err);
+    if(result != undefined)
+      return res.json("Insert was successful");
+    });
+
+}
+
 module.exports = {
-  getAllUsers
+  getAllUsers,
+  getUserByLastName,
+  createUser
 };

7527710777663ee024ae319b900fc71430d4c4bd
jwechsler10 <steamacct2@gmail.com>
2018-02-13 11:17:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.connect()
  console.log(err);
 });
 
-client.execute("INSERT INTO users.users (uuid(), lastname, age, city, email, firstname) VALUES ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')", function (err, result) {
+client.execute("INSERT INTO users.users (id, lastname, age, city, email, firstname) VALUES (uuid(), 'Jones', 35, 'Austin', 'bob@example.com', 'Bob')", function (err, result) {
             // Run next function in series
               if(err)
                console.log(err);
